8fced1e2b42d5343c22f1c61db8029439a75a9c3
Rok Strucelj <rok.coko@gmail.com>
2020-05-19 14:42:06 +0200

    Renamed Files

--- app/src/main/java/si/uni_lj/fe/gamehelperapp/GameSelectActivity.java
+++ app/src/main/java/si/uni_lj/fe/gamehelperapp/GameSelectActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class GameSelectActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.gameselect_activity);
     }
 }
